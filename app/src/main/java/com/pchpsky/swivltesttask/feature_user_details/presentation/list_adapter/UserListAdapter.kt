34ab9889053a209be96509233bf1502c37d3dd5a
Camila <camilaabalova@gmail.com>
2022-02-11 15:24:38 +0200

    recycler adapter with viewbinding

--- app/src/main/java/com/pchpsky/swivltesttask/feature_user_details/presentation/list_adapter/UserListAdapter.kt
+++ app/src/main/java/com/pchpsky/swivltesttask/feature_user_details/presentation/list_adapter/UserListAdapter.kt
@@ -1,10 +1,9 @@
 package com.pchpsky.swivltesttask.feature_user_details.presentation.list_adapter
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.pchpsky.swivltesttask.R
+import com.pchpsky.swivltesttask.databinding.ItemUserBinding
 import com.pchpsky.swivltesttask.feature_user_details.domain.model.User
 
 class UserListAdapter(private val users: List<User>) : RecyclerView.Adapter<UserListAdapter.UsersViewHolder>() {
@@ -12,13 +11,15 @@ class UserListAdapter(private val users: List<User>) : RecyclerView.Adapter<User
     override fun getItemCount(): Int = users.count()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UsersViewHolder {
-        return UsersViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_user, parent, false))
+        val binding = ItemUserBinding
+            .inflate(LayoutInflater.from(parent.context), parent, false)
+        return UsersViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: UsersViewHolder, position: Int) = holder.bind(users[position])
 
-    inner class UsersViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        fun bind(user: User) = with(itemView) {
+    inner class UsersViewHolder(private val userListView: ItemUserBinding) : RecyclerView.ViewHolder(userListView.root) {
+        fun bind(user: User) = with(userListView) {
 
         }
     }
